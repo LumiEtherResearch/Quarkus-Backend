1c91df78dc5405f251e7fefea99a78896ed86615
Adarsh <adarsh.bhaskar7903@gmail.com>
2025-03-01 14:34:05 +0530

    Added Main Manifest for the project

--- src/main/java/com/example/MainApplication.java
+++ src/main/java/com/example/MainApplication.java
@@ -1,7 +1,9 @@
 package com.example;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 
+@QuarkusMain
 public class MainApplication implements QuarkusApplication {
 
 	@Override
